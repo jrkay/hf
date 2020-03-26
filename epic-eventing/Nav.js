b46ac307a1563623304e2cefe219bafff82496df
jrkay <jrkohlmeyer@gmail.com>
2020-03-26 14:41:39 -0700

    Initial commit

---
+++ epic-eventing/Nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Col } from 'reactstrap';
+
+class Nav extends Component {
+	render() {
+		return (
+            <div style={{display: "flex"}}>
+
+            </div>
+        );
+    }
+} 
+export default Nav;
